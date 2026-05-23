943194941694d701a3ce8be830e02e97c5d461c0
GustavooPerini <gustavooperini@gmail.com>
2026-05-23 20:51:06 -0300

    Otimizando o algoritmo GA

--- GA/main.py
+++ GA/main.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 N = 30
-POPULACAO = 100
+N_POPULACAO = 30
+N_GERACOES = 20
 LOW = -32
 HIGH = 32
 
-def rastrigin_like(x):
+def fitness(x):
+    
+    punicao = 1
+
     x = np.asarray(x)
     n = x.size
 
@@ -17,7 +21,75 @@ def rastrigin_like(x):
         np.sum(np.cos(2 * np.pi * x)) / n
     )
 
-    return term1 + term2 + 20 + np.e
+    if x[np.argmin(x)] < LOW or x[np.argmax(x)] > HIGH:
+        punicao = 100
+
+    return (term1 + term2 + 20 + np.e) * punicao
 
 def gera_populacao():
-    return np.random.uniform(low=LOW, high=HIGH, size=(POPULACAO, N))
+    return np.random.uniform(low=LOW, high=HIGH, size=(N_POPULACAO, N))
+
+def selecao(populacao, fitness_arr):
+    nova_populacao = np.zeros(shape=(N_POPULACAO, N), dtype=float)
+
+    for i in range(N_POPULACAO):
+        ind1_idx = np.random.randint(low=0, high=N_POPULACAO)
+        ind2_idx = np.random.randint(low=0, high=N_POPULACAO)
+
+        if fitness_arr[ind1_idx] < fitness_arr[ind2_idx]:
+            nova_populacao[i] = populacao[ind1_idx]
+        else:
+            nova_populacao[i] = populacao[ind2_idx]
+
+    return nova_populacao
+
+def cruzamento(populacao, taxa_cruzamento=0.7):
+    nova_populacao = np.zeros(shape=(N_POPULACAO, N), dtype=float)
+
+    for i in range(N_POPULACAO // 2):
+        pai1_idx = np.random.randint(low=0, high=N_POPULACAO)
+        pai2_idx = np.random.randint(low=0, high=N_POPULACAO)
+
+        pai1 = populacao[pai1_idx]
+        pai2 = populacao[pai2_idx]
+
+        if np.random.random() <= taxa_cruzamento:
+            beta = np.random.normal(loc=0, scale=1)
+            pai1 = (beta * pai1) + ((1 - beta) * pai2)
+            pai2 = ((1 - beta) * pai1) + (beta * pai2)
+        
+        nova_populacao[i*2] = pai1
+        nova_populacao[(i*2) + 1] = pai2
+    
+    return nova_populacao
+
+def mutacao(populacao, taxa_mutacao=0.1):
+    
+    for i in range(N_POPULACAO):
+        if np.random.random() <= taxa_mutacao:
+            alfa = np.random.normal(loc=0, scale=1)
+            populacao[i] = populacao[i] * alfa
+
+    
+
+populacao = gera_populacao()
+for i in range(N_GERACOES):
+
+    # Aplicando a fitness em todos os indivíduos
+    fitness_arr = np.array([fitness(ind) for ind in populacao])
+
+    # Pegando o indivíduo com o menor fitness dessa população
+    melhor_idx = np.argmin(fitness_arr)
+    melhor_indv = np.copy(populacao[melhor_idx])
+
+    populacao = selecao(populacao, fitness_arr)
+    populacao = cruzamento(populacao)
+    mutacao(populacao)
+
+    # Aplicando o elitismo
+    populacao[0] = melhor_indv
+
+fitness_final = np.array([fitness(ind) for ind in populacao])
+melhor_idx = np.argmin(fitness_final)
+
+print("Melhor fitness encontrado: ", fitness_final[melhor_idx])
